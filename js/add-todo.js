8ce018d6b75d8ff66d3f9852cfe2a78f9fa2beb1
Cari Palazzolo <cari.pizza@gmail.com>
2018-11-10 13:20:12 -0800

    updated add-todo.js with new component architecture linked dom to app.js

--- js/add-todo.js
+++ js/add-todo.js
@@ -1,25 +1,78 @@
+import html from './html.js';
 
-const addTodo = {
-    init(onAdd) {
-        const form = document.getElementById('add-todo-form');
+function makeTodo() {
+    return html`
+        <form id="add-todo-form">
+            <label>
+                Task:<br>
+                <input maxlength="30" name="task" required>
+            </label>
+            <label>
+                Due date:<br>
+                <input type="date" name="due" id="due-date-input" required>
+            </label>
+            <label class="hidden">
+                Done:<br>
+                <input type="checkbox" name="done" class="hidden">
+            </label>
+            <p>
+                <button class="action">Add New</button> 
+            </p>
+        </form>
+        <hr />
+    `;
+}
 
-        form.addEventListener('submit', function(event) {
+class AddTodo {
+    constructor(onAdd) {
+        this.onAdd = onAdd;
+    }
+
+    render() {
+        const dom = makeTodo();
+        const form = dom.getElementById('add-todo-form');
+        form.addEventListener('submit', event => {
             event.preventDefault();
             const elements = form.elements;
-            
             const todo = {
                 task: elements.task.value,
                 due: elements.due.value,
                 done: elements.done.checked,
             };
 
-            onAdd(todo); // child is saying hey parent do this
+            this.onAdd(todo);
 
             form.reset();
             document.activeElement.blur();
         });
 
+        return dom;
     }
-};
+}
 
-export default addTodo;
+export default AddTodo;
+
+// const addTodo = {
+//     init(onAdd) {
+//         const form = document.getElementById('add-todo-form');
+
+//         form.addEventListener('submit', function(event) {
+//             event.preventDefault();
+//             const elements = form.elements;
+            
+//             const todo = {
+//                 task: elements.task.value,
+//                 due: elements.due.value,
+//                 done: elements.done.checked,
+//             };
+
+//             onAdd(todo); // child is saying hey parent do this
+
+//             form.reset();
+//             document.activeElement.blur();
+//         });
+
+//     }
+// };
+
+// export default addTodo;
--- js/app.js
+++ js/app.js
@@ -1,22 +1,23 @@
-import html from './html.js';
-import todoList from './todo-list.js';
 import todoApi from './todo-api.js';
-import addTodo from './add-todo.js';
+import html from './html.js';
 import Header from './header.js';
+import AddTodo from './add-todo.js';
+import todoList from './todo-list.js';
 
 const todos = todoApi.getAll();
 
 function makeTemplate() {
     return html`
     <header></header>
+    <div id="add-todo"></div>
     `;
 }
 
 
-addTodo.init(function(todo) {
-    todoApi.add(todo);
-    todoList.add(todo);
-});
+// addTodo.init(function(todo) {
+//     todoApi.add(todo);
+//     todoList.add(todo);
+// });
 
 
 
@@ -28,9 +29,20 @@ todoList.init(todos, function(todo) {
 class TodoApp {
     render() {
         const dom = makeTemplate();
+
         const headerSection = dom.querySelector('header');
+        const addTodoSection = dom.getElementById('add-todo');
+
         const header = new Header();
         headerSection.appendChild(header.render());
+        
+        const addTodo = new AddTodo(todo => {
+            todoApi.add(todo);
+            todoList.add(todo);
+        });
+        addTodoSection.appendChild(addTodo.render());
+
+        
         return dom;
     }
 }
