588cf4f1e42256595ae241da205bf059ac95ec4d
Cari Palazzolo <cari.pizza@gmail.com>
2018-11-10 18:27:17 -0800

    removed unused notes and unused newtodo-list.js file

--- js/add-todo.js
+++ js/add-todo.js
@@ -1,5 +1,4 @@
 import html from './html.js';
-// import TodoListSection from './newtodo-list.js';
 
 function makeTodoForm() {
     return html`
@@ -21,7 +20,6 @@ function makeTodoForm() {
             </p>
         </form>
         <hr />
-        <!--<div id="todo-section"></div>-->
     `;
 }
 
@@ -47,9 +45,6 @@ export default class AddTodo {
             form.reset();
             document.activeElement.blur();
         });
-        // const container = dom.querySelector('#todo-section');
-        // const todoSection = new TodoListSection();
-        // container.appendChild(todoSection.render());
 
         return dom;
     }
